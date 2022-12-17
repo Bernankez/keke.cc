6da4eeec4a0ec51aef1a9a53cf56307fdce7be1a
cole <bernankez@qq.com>
2022-12-17 23:45:10 +0800

    chore: enable reactivity transform

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,11 +29,9 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
 defineProps<{ msg: string }>();
 
-const count = ref(0);
+const count = $ref(0);
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    Unocss(),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
