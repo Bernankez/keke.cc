c441966e72e4a1fec029567befd472f091222eab
cole <bernankez@qq.com>
2023-04-14 11:31:34 +0800

    feat(style): add view transition style

--- src/components/ModeSwitch.vue
+++ src/components/ModeSwitch.vue
@@ -11,7 +11,44 @@ import { isDark } from "@/composables/dark";
 
 const icon = computed(() => isDark.value ? "i-ri:moon-line" : "i-ri:sun-line");
 const toggle = useToggle(isDark);
-const toggleMode = () => {
-  toggle();
+
+const toggleMode = (event: MouseEvent) => {
+  if ("startViewTransition" in document) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const endRadius = Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y),
+    );
+
+    const viewTransition = (document as any).startViewTransition(() => {
+      toggle();
+      const html = document.querySelector("html");
+      if (html) {
+        html.classList.remove(isDark.value ? "dark" : "light");
+        html.classList.add(isDark.value ? "light" : "dark");
+      }
+    });
+
+    viewTransition.ready.then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ];
+
+      document.documentElement.animate(
+        {
+          clipPath: isDark.value ? clipPath : [...clipPath].reverse(),
+        },
+        {
+          duration: 500,
+          easing: "ease-in",
+          pseudoElement: isDark.value ? "::view-transition-new(root)" : "::view-transition-old(root)",
+        },
+      );
+    });
+  } else {
+    toggle();
+  }
 };
 </script>
