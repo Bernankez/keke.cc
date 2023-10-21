259e6d222f2d29c19cc4282a5915441171df5e8d
cole <bernankez@qq.com>
2023-10-22 02:22:49 +0800

    fix(Mask): prevent click propagation

--- src/components/Mask.vue
+++ src/components/Mask.vue
@@ -1,7 +1,7 @@
 <template>
   <DefineTemplate>
     <div v-if="show" :style="{ zIndex }" class="fixed bottom-0 left-0 right-0 top-0 bg-darkbackground bg-opacity-50 backdrop-blur-10 backdrop-saturate-50 transition-300" @click="emit('click', $event)">
-      <div class="relative left-50% top-50% h-fit w-fit -translate-x-50% -translate-y-50%">
+      <div class="relative left-50% top-50% h-fit w-fit -translate-x-50% -translate-y-50%" @click.stop>
         <slot></slot>
       </div>
     </div>
