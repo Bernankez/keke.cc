9796ffad47ff2caf63878729247602ad5f535252
cole <bernankeic@gmail.com>
2024-01-19 15:14:27 +0800

    feat: add projects

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div :style="{ zIndex }" class="absolute left-0 top-0 box-border h-18 w-full flex items-center justify-between bg-transparent bg-opacity-70 p-x-5 dark:bg-opacity-70 lg:backdrop-blur-8 lg:backdrop-saturate-50">
     <div class="flex items-center flex-gap-3">
-      <Icon icon="i-ri:calendar-2-line" title="DayGram" @click="show = !show" />
+      <!-- <Icon icon="i-ri:calendar-2-line" title="DayGram" @click="show = !show" /> -->
       <Calendar v-model="show" close-on-mask />
     </div>
     <div class="flex items-center flex-gap-3">
@@ -12,7 +12,7 @@
 </template>
 
 <script setup lang="ts">
-const show = ref(true);
+const show = ref(false);
 const { use } = useZIndex();
 const zIndex = ref(use());
 </script>
--- src/views/home/bottom.vue
+++ src/views/home/bottom.vue
@@ -16,9 +16,17 @@
   <div class="text-7">
     <span class="text-primary">Libraries</span>
     <div class="grid grid-cols-1 m-t-2 grid-gap-2 md:grid-cols-2">
-      <Tagi title="git-sync" icon="i-gridicons:create" desc="TypeScript library scaffold." href="https://github.com/Bernankez/create-l" />
+      <Tagi title="git-sync" icon="i-tabler:git-branch" desc="Auto sync repos when pushing to git." href="https://github.com/Bernankez/git-sync" />
       <Tagi title="create-l" icon="i-gridicons:create" desc="TypeScript library scaffold." href="https://github.com/Bernankez/create-l" />
       <Tagi title="@bernankez/utils" icon="i-ph:package" desc="Using utils out-of-the-box" href="https://utils.keke.cc/" />
+    </div>
+  </div>
+  <div class="text-7">
+    <span class="text-primary">Presets</span>
+    <div class="grid grid-cols-1 m-t-2 grid-gap-2 md:grid-cols-2">
+      <Tagi title="vscode-settings" icon="i-nonicons:vscode-16" desc="My VSCode settings." href="https://github.com/Bernankez/vscode-settings" />
+      <Tagi title="eslint-config" icon="i-nonicons:eslint-16" desc="My ESLint config presets." href="https://github.com/Bernankez/eslint-config" />
+      <Tagi title="stylelint-config (Outdated)" icon="i-logos:stylelint" desc="My Stylelint config presets." href="https://github.com/Bernankez/stylelint-config" />
     </div>
   </div>
 </template>
