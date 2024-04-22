6eba190760de4e29fa7760ca3ce20d3e08d80585
cole <bernankeic@gmail.com>
2024-04-22 15:14:55 +0800

    chore: remove vite-ssg

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { ViteSSG } from "vite-ssg";
 import "./style.css";
 import "virtual:uno.css";
 import App from "./App.vue";
-import { routes } from "./router";
+import { router } from "./router";
 
-export const createApp = ViteSSG(App, { routes });
+createApp(App).use(router).mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import type { RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
-export const routes: RouteRecordRaw[] = [
+const routes: RouteRecordRaw[] = [
   {
     path: "/",
     component: () => import("@/views/home/index.vue"),
   },
 ];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
