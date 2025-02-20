699f98e3f7f9b92f9e19e2dc51b387f299db15b2
cole <bernankeic@gmail.com>
2025-02-20 18:31:11 +0800

    feat: add tracking entry

--- src/views/home/top.vue
+++ src/views/home/top.vue
@@ -11,6 +11,7 @@
     </div>
     <div class="flex flex-gap-3">
       <Icon href="https://blog.keke.cc/" title="Posts" icon="i-ri:bill-line" show="auto" />
+      <Icon href="https://tracking.keke.cc/" title="Tracking" icon="i-ri:focus-2-line" show="auto" />
       <Icon href="https://github.com/Bernankez" title="GitHub" icon="i-ri:github-line" show="auto" />
       <Icon icon="i-ri:bilibili-fill" title="Bilibili" href="https://space.bilibili.com/32431375" show="auto" />
     </div>
